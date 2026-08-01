9f96c60c46c2adcea3b4d6040e85cb794db806bc
Lucas Vilela <lrvilelab@hotmail.com>
2026-08-01 15:20:54 -0300

    feat: added ensure authenticated

---
+++ src/middlewares/ensure-authenticated.ts
@@ -0,0 +1,35 @@
+// Garante que tenha acesso as rotas somente se estiver autenticado.
+
+import { verify } from "jsonwebtoken";
+import { authConfig } from "@/configs/auth";
+import { AppError } from "@/utils/AppError";
+import { Request, Response, NextFunction } from "express";
+
+interface TokenPayload {
+  role: string
+  sub: string
+}
+
+export function ensureAuthenticated(req: Request, res: Response, next: NextFunction) {
+  try {
+    const authHeader = req.headers.authorization
+
+    if (!authHeader) {
+      throw new AppError("JWT token not found!", 401)
+    }
+
+    const [, token] = authHeader.split(" ")
+
+    const { role, sub: user_id } = verify(token, authConfig.jwt.secret) as TokenPayload
+
+    req.user = {
+      id: user_id,
+      role
+    }
+
+    return next()
+
+  } catch (error) {
+      throw new AppError("Invalid JWT token!", 401)
+  }
+}
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { usersRoutes } from "./users-routes";
 import { sessionsRoutes } from "./sessions-routes";
 import { refundsRoutes } from "./refunds-routes";
+import { ensureAuthenticated } from "@/middlewares/ensure-authenticated";
 
 const routes = Router()
 
@@ -10,6 +11,7 @@ routes.use("/users", usersRoutes)
 routes.use("/sessions", sessionsRoutes)
 
 // Rotas privadas.
+routes.use(ensureAuthenticated)
 routes.use("/refunds", refundsRoutes)
 
 export { routes }
