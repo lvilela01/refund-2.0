8bce2a8761637ec6d62d97f81d7d0cbc6bccf39e
Lucas Vilela <lrvilelab@hotmail.com>
2026-08-04 00:01:18 -0300

    feat: pagination

--- src/controllers/refunds-controller.ts
+++ src/controllers/refunds-controller.ts
@@ -33,12 +33,19 @@ export class RefundsController{
 
   async index(req: Request, res: Response){
     const querySchema = z.object({
-      name: z.string().optional().default("")
+      name: z.string().optional().default(""),
+      page: z.coerce.number().optional().default(1),
+      perPage: z.coerce.number().optional().default(10),
     })
 
-    const { name } = querySchema.parse(req.query)
+    const { name, page, perPage } = querySchema.parse(req.query)
+
+    // Calcula os valores de skip.
+    const skip = (page - 1) * perPage
 
     const refunds = await prisma.refunds.findMany({
+      skip,
+      take: perPage,
       where: {
         user: {
           name: {
@@ -50,6 +57,24 @@ export class RefundsController{
       include: { user: true }
     })
 
-    res.json(refunds)
+    // Obter o total de registros para calcular o número de páginas.
+    const totalRecords = await prisma.refunds.count({
+      where: {
+        user: {
+          name: {
+            contains: name.trim()
+          }
+        }
+      }
+    })
+
+    const totalPages = Math.ceil(totalRecords / perPage)
+
+    res.json({
+      refunds,
+      pagination: {
+        page, perPage, totalRecords, totalPages: totalPages > 0 ? totalPages : 1
+      }
+    })
   }
 }
