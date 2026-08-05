b9fc4c4d9ff80a15c7bbd944c036085a14a7c9f4
Lucas Vilela <lrvilelab@hotmail.com>
2026-08-05 01:41:50 -0300

    feat: The file validation controller is now complete (checking whether the file was attached, as well as its size and type). A method has also been created to handle this file—saving or deleting it depending on the validation results.

--- src/configs/upload.ts
+++ src/configs/upload.ts
@@ -5,7 +5,10 @@ import crypto from "node:crypto"
 const TMP_FOLDER = path.resolve(__dirname, "..", "..", "tmp")
 const UPLOADS_FOLDER = path.resolve(TMP_FOLDER, "uploads")
 
-const MAX_FILE_SIZE = 1024 * 1024 * 3 // 3MB
+const MAX_SIZE = 3 // 3 é o quanto de MB permite.
+// 1KB = 1024 Bytes.
+// 1MB = 1024²
+const MAX_FILE_SIZE = 1024 * 1024 * MAX_SIZE
 const ACCEPTED_IMAGE_TYPES = ["image/jpeg", "image/jpg", "image/png"]
 
 const MULTER = {
@@ -20,10 +23,11 @@ const MULTER = {
   })
 }
 
-export {
+export default {
   TMP_FOLDER,
   UPLOADS_FOLDER,
   MULTER,
+  MAX_SIZE,
   MAX_FILE_SIZE,
   ACCEPTED_IMAGE_TYPES
 }
---
+++ src/routes/uploads-routes.ts
@@ -0,0 +1,18 @@
+import { Router } from "express";
+import { UploadsController } from "@/controllers/uploads-controller";
+import { verifyUserAuthorization } from "@/middlewares/verify-user-authorization";
+
+import multer from "multer"
+import uploadConfig from "@/configs/upload"
+
+
+
+const uploadsRoutes = Router()
+const uploadsController = new UploadsController()
+
+const upload = multer(uploadConfig.MULTER)
+
+uploadsRoutes.use(verifyUserAuthorization(["employee"]))
+uploadsRoutes.use("/", upload.single("file"), uploadsController.create)
+
+export { uploadsRoutes }
